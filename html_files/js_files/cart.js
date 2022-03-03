9c890c3a7801f39c088c8993437863bdf9ca4946
Nithin G Shastry <nithingshastry@gmail.com>
2022-03-03 17:42:02 +0530

    CHANGES

--- html_files/js_files/cart.js
+++ html_files/js_files/cart.js
@@ -1,14 +1,27 @@
 var recieved = [];
 
-function addToCart(){
+window.addEventListener('load', function() {
     recieved.push(localStorage.getItem('greeting'));
     console.log(recieved);
     var str = recieved[recieved.length - 1];
     var elements = str.split(":");
     var temp = "";
     for(var i = 0 ; i < elements.length ; i++){
-        temp += elements[i] + '<br>';
+        temp += elements[i] + '<button type="button" class="item_button">+</button><br>';
         console.log(temp);
     }
     document.getElementById('items').innerHTML = temp;
-}
+})
+
+// function addToCart(){
+//     recieved.push(localStorage.getItem('greeting'));
+//     console.log(recieved);
+//     var str = recieved[recieved.length - 1];
+//     var elements = str.split(":");
+//     var temp = "";
+//     for(var i = 0 ; i < elements.length ; i++){
+//         temp += elements[i] + '<br>';
+//         console.log(temp);
+//     }
+//     document.getElementById('items').innerHTML = temp;
+// }
